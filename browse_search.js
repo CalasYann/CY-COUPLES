95b2d6a05a6278e59ae1a14b128360cc354d7e43
Nathan Le Nestour <le_nestn@outlook.fr>
2024-05-16 01:10:03 +0200

    async function

--- browse_search.js
+++ browse_search.js
@@ -1,3 +1,11 @@
 const form = document.querySelector("#browsing_info");
 
+
+async function sendData(){
+    const formData = new FormData(form);
+
+    const response = await fetch("*****.php");
+
+}
+
 //récupérer les informations du formulaire et les envoyer vers un fichier php : https://developer.mozilla.org/en-US/docs/Learn/Forms/Sending_forms_through_JavaScript
