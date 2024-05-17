5156beefca5fbccdc1ae1fa522093ac96998d414
CalasYann <133207210+CalasYann@users.noreply.github.com>
2024-05-17 11:46:45 +0200

    Update Profils.php

--- Profils.php
+++ Profils.php
@@ -10,7 +10,10 @@
 <?php
 	require_once ('define.inc.php');
 	require_once ('fonctionmodif.php');
-	
+
+    if($_Get["error"]==1){
+    	echo("Modification Impossible");
+    }
     affichageProfil(1);
 	
 ?>
