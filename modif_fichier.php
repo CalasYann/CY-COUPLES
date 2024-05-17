3e7dc4aa65ddfcf29be8c33014ee34266ebc3fd1
CalasYann <133207210+CalasYann@users.noreply.github.com>
2024-05-17 11:47:13 +0200

    Update modif_fichier.php

--- modif_fichier.php
+++ modif_fichier.php
@@ -6,5 +6,67 @@
 
 //rediriger vers profils+rajouter dans l'url un message d'erreur si besoin
 
+function create_tab(){
+	$tab=array(
+		"NOM" => $_POST['Nom'],
+		"PRENOM" => $_POST['Prenom'],
+		"PSEUDO" => $_POST['Pseudo'],
+		"BRAWLNAME" => $_POST["Bname"],
+		"PASSWORD" => $_POST["Mdp"],
+		"MAIL" => $_GET["mail"],
+		"ID" => $_POST["Id"],
+		"BRAWLER" => $_POST["Perso"],
+		"MODE" => $_POST["Mode"],
+	);
+	
+	foreach($tab as $key => $value){
+		//echo("$key : $value"."</br>");
+	}
+	
+	return $tab;
+}
+
+
+
+function verif_form($tab){
+	return true;
+}
+
+
+function update($tab){
+	
+	if(verif_form($tab)){
+		
+		$file =fopen("backend/".$_GET['mail']."/private.txt", "w");
+		if($file!==false){
+			$hello=1;
+			$error=0;
+			foreach($tab as $key => $value){
+				$hello=fwrite($file, "$key:$value"."\n");
+			
+			}		
+		}
+	}	
+	else{
+		$error=1;
+	}
+	
+	header('Location: Profils.php?error='.$error);
+}
+
+
+$tab=create_tab();
+
+update($tab);
+
+
+
+
+
+
+
+
+
+
 
 ?>
