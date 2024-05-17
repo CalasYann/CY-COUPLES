bfb5bdfad79646ef7cd797c1d0cb041346950138
nathan-le-nestour <154378668+nathan-le-nestour@users.noreply.github.com>
2024-05-17 11:49:55 +0200

    Update test_browsing_players.php

--- test_browsing_players.php
+++ test_browsing_players.php
@@ -1,43 +1,34 @@
 <?php
 
-    require_once("browse_players.php");
-    require_once("liste_brawlers.php");
-
+require_once("liste_brawlers.php");
 
 ?>
 
 <!DOCTYPE html>
-
 <html>
     <head>
-        <script src="browse_serach.js"></script>
-        <title>Recherche de Mates</title>
+        <title>Recherche</title>
+        <script src ="test.js">
+        </script>
     </head>
     <body>
-        <div>
-            <form id="browsing_info">
-                <input type="text" id="text-bar" name="text-bar">
-                <label for="brawler">Brawler :</label>
-                <input list="brawler" name="brawler"/>
-                <datalist id="brawler">
-                    <?php
-                        display_brawlers();
-                    ?>
-                </datalist>
+        <form id="browsing_data" action="test_browse2.php" method="get">
+            <input id="mode_field" name="mode" list="mode" placeholder="Mode préféré">
+            <datalist id ="mode">
+                <option value="bb">Brawlball</option>
+				<option value="rz">Razzia</option>
+				<option value="sv">Survivant</option>
+				<option value="bq">Braquage</option>
+				<option value="hj">Hors-Jeu</option>  
+            </datalist>
 
-                <label for="mode">Mode de jeu préféré :</label>
-                <input list="mode" name="mode">
-                <datalist id="mode">
-                    <option value="bb">Brawlball</option>
-                    <option value="rz">Razzia</option>
-                    <option value="sv">Survivant</option>
-                    <option value="bq">Braquage</option>
-                    <option value="hj">Hors-Jeu</option>
-                </datalist>
-
-                <input type="submit" value="Rechercher"/>
-
-            </form>
-        </div>
+            <input id="brawler_field" list="brawler" name="brawler" placeholder="Brawler">
+            <datalist id="brawler">
+                <?php
+                    display_brawlers();
+                ?>
+            </datalist>
+            <input type="button" value="Rechercher" onclick="sendData()">
+        </form>
     </body>
 </html>
