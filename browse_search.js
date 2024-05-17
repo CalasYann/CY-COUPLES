5265290609d0d8276d11d4ec4db7d0aa6372f1f9
nathan-le-nestour <154378668+nathan-le-nestour@users.noreply.github.com>
2024-05-17 11:45:34 +0200

    Update browse_search.js
    xmlhttpRequest

--- browse_search.js
+++ browse_search.js
@@ -1,11 +1,28 @@
-const form = document.querySelector("#browsing_info");
 
 
-async function sendData(){
-    const formData = new FormData(form);
+function sendData(){
 
-    const response = await fetch("*****.php");
+    var xhr = new XMLHttpRequest();
+
+    xhr.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200){
+            console.log(this.responseText);
+
+            // json parse ???
+            // 'foreach'  : var users[i].pseudo .xxx
+        }
+    };
+    
+    let mode    = document.getElementById("mode_field");
+    let brawler = document.getElementById("brawler_field");
+
+    console.log(mode);
+
+    params  = "mode=" + mode.value;
+    params += "&brawler=" + brawler.value;
+
+    xhr.open("POST", "test_browse2.php", true);
+    xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xhr.send(params);
 
 }
-
-//récupérer les informations du formulaire et les envoyer vers un fichier php : https://developer.mozilla.org/en-US/docs/Learn/Forms/Sending_forms_through_JavaScript
