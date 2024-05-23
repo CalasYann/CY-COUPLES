bee54b270329432364b1245975706c35ddae4f68
Nathan Le Nestour <le_nestn@outlook.fr>
2024-05-24 01:18:37 +0200

    page.css

---
+++ page.php
@@ -0,0 +1,16 @@
+<?php
+
+require_once("get_user_info.php");
+
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title></title>
+        <link rel="stylesheet" href="page.css">
+    </head>
+    <body>
+        <section id="topbar"><a href="hub.php" class="topbar_elm">Accueil</a></section>
+    </body>
+</html>
