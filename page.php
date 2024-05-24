23fb73e708a26c25ec08394d33c1e02a6e58138d
nathan-le-nestour <154378668+nathan-le-nestour@users.noreply.github.com>
2024-05-24 11:51:05 +0200

    Update page.php

--- page.php
+++ page.php
@@ -1,6 +1,6 @@
-<?php
+<div?php
 
-require_once("get_user_info.php");
+//require_once("get_user_info.php");
 
 ?>
 
@@ -11,6 +11,15 @@ require_once("get_user_info.php");
         <link rel="stylesheet" href="page.css">
     </head>
     <body>
-        <section id="topbar"><a href="hub.php" class="topbar_elm">Accueil</a></section>
+        <div id="topbar"><a href="https://google.com" class="topbar_home">Accueil</a></div>
+        <section>
+            <div id="profile">
+                <div>
+                    <img  id="player_icon" src="https://preview.redd.it/show-penny-some-love-v0-j9w5ibmxunjc1.jpeg?auto=webp&s=694a604721725b039a43ccb69f04630155e7b848" alt="">
+                </div>
+                <div id="player_nick">player nick</div>
+                
+            </div>
+        </section>
     </body>
 </html>
